6197d91b5d6e98cdf44971af63cdec41ed6cfe4d
Md-shefat-masum <myphoto240@gmail.com>
2019-09-22 00:50:55 +0600

    --- Auto-Git Commit ---

--- js/custom.js
+++ js/custom.js
@@ -106,6 +106,25 @@ $(document).ready(function () {
         }
     });
 
+    $(".video-gallery").owlCarousel({
+        items: 1,
+        nav: false,
+        dots: false,
+        autoplay: true,
+        loop: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+
     $('.venobox').venobox({
         titleattr : 'data-title',
         numeratio: true,
